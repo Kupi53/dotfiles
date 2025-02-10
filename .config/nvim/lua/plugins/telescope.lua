2d164cd1732593765bd91ac58ddb0c32add54c5b
Kupi53 <146436726+Kupi53@users.noreply.github.com>
2025-02-10 21:04:59 +0100

    [MODIFIED] telescope plugin in neovim for finding hidden files, [ADD] zsh config through oh my zsh plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,45 +4,55 @@ return {
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-fzf-native.nvim' },
     config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files)
-    end
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {}
-          },
-        },
+      local telescope = require("telescope")
+      local builtin = require("telescope.builtin")
+
+      telescope.setup({
         defaults = {
+          vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden',  -- Enable hidden files search
+            '--glob', '!.git/*'  -- Exclude .git directory
+          },
           mappings = {
             i = {
-              ['<C-k>'] = require("telescope").move_selection_previous,
-              ['<C-j>'] = require("telescope").move_selection_next,
-              ['<C-l>'] = require("telescope").move_selection_default,
+              ['<c-k>'] = require("telescope.actions").move_selection_previous,
+              ['<c-j>'] = require("telescope.actions").move_selection_next,
+              ['<c-l>'] = require("telescope.actions").select_default,
             },
           },
         },
+        pickers = {
+          find_files = {
+            hidden = true,  -- Ensure find_files shows hidden files
+            no_ignore = true, -- Optional: Show files ignored by .gitignore
+            follow = true, -- Follow symbolic links
+          },
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {},
+          },
+        },
       })
-      require("telescope").load_extension("ui-select")
+
+      -- Load extensions
+      telescope.load_extension("ui-select")
+
+      -- Keybindings
+      vim.keymap.set('n', '<leader>ff', function()
+        builtin.find_files({ hidden = true, no_ignore = true })
+      end, { desc = "Find files (including dotfiles)" })
     end
   },
   {
     'nvim-telescope/telescope-ui-select.nvim',
-    config = function()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      }
-      require("telescope").load_extension("ui-select")
-    end
-  },
+  }
 }
 
